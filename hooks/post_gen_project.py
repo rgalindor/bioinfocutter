f2f68e952773fbaf30334fd3757ae681649e5e87
rgalindor <rgalindo.biocomputo@gmail.com>
2023-08-05 03:08:19 -0600

    feat: adding git authoring

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,7 +9,9 @@ print(f"{MESSAGE_COLOR}Almost done!")
 print(f"Initializing a git repository ...{RESET_ALL}")
 
 subprocess.call(['git', 'init'])
-subprocess.call(['git', 'add', '*'])
+subprocess.call(['git', 'config', '--local', 'user.name', '"{{ cookiecutter.git_username }}"'])
+subprocess.call(['git', 'config', '--local', 'user.email', '"{{ cookiecutter.git_email}}"'])
+subprocess.call(['git', 'add', '-A'])
 subprocess.call(['git', 'commit', '-m', 'Initial commit'])
 
 print(f"{MESSAGE_COLOR}The beggining of your project is defined now!\nBye {author}!{RESET_ALL}")
